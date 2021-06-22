0db15e6b1f845f187c0c00d3a6e9e122b53ab9a5
iotti0 <82509628+iotti0@users.noreply.github.com>
2021-06-22 12:54:30 +0700

    added update html

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['inter var', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe-UI', 'Arial', 'sans-serif'],
+      },
+      maxWidth: {
+        xs: '21rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
